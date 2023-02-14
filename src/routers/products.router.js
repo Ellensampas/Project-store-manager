fc626d71c09992a10827c8e3aeb092dcdf49ffdb
ellen Sampaio <ellenssbarross@gmail.com>
2023-02-14 13:12:52 -0300

    req4 - concluido

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const { validaName } = require('../middlewares/validaName');
 
 const router = express.Router();
 
 router.get('/', productsController.listAllProducts);
 router.get('/:id', productsController.getProductsId);
-router.post('/', productsController.insertProd);
+router.post('/', validaName, productsController.insertProd);
 
 module.exports = router;
